2e7c4be0965bd25eca182ea343b9bd24a5b55d00
Meteor Liu <meteor1113@gmail.com>
2010-05-04 09:16:20 +0800

    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- sample-proj.el
+++ sample-proj.el
@@ -1,18 +1,21 @@
-(ede-cpp-root-project "library"
-                      :file (or load-file-name (buffer-file-name))
-                      :include-path '("/"
-                                      "/dumpxml"
-                                      "/dumpxml/boost"
-                                      "/dumpxml/tinyxml"
-                                      "/encrypt"
-                                      "/libsock/include"
-                                      "/libthread/include"
-                                      "/libutil/include"
-                                      "/loki"
-                                      "/mail"
-                                      "/win"
-                                      )
-                      ;; :system-include-path '("/user/include")
-                      ;; :spp-table '(("isUnix" . "")
-                      ;;              ("BOOST_TEST_DYN_LINK" . ""))
-                      )
+(when (fboundp 'ede-cpp-root-project)
+  (ede-cpp-root-project "library"
+                        :name "cpp-library"
+                        :version "0.2"
+                        :file (or load-file-name (buffer-file-name))
+                        :include-path '("/"
+                                        "/dumpxml"
+                                        "/dumpxml/boost"
+                                        "/dumpxml/tinyxml"
+                                        "/encrypt"
+                                        "/libsock/include"
+                                        "/libthread/include"
+                                        "/libutil/include"
+                                        "/loki"
+                                        "/mail"
+                                        "/win"
+                                        )
+                        ;; :system-include-path '("/user/include")
+                        ;; :spp-table '(("isUnix" . "")
+                        ;;              ("BOOST_TEST_DYN_LINK" . ""))
+                        ))
