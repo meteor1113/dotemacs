09fc540e424415effa096c99d9d7ed02e2b3c773
Meteor Liu <meteor1113@gmail.com>
2010-05-30 14:46:58 +0800

    Update toolbar setting.
    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- init-toolbar.el
+++ init-toolbar.el
@@ -18,21 +18,33 @@
 (setq tool-bar-button-margin 0)
 ;; (setq auto-resize-tool-bars nil)
 
+(defvar show-bm-tool-bar nil
+  "If not nil, while show bm toolbar, else hide bm toolbar.")
+
+(defun toggle-bm-tool-bar ()
+  "Toggle bm toolbar show."
+  (interactive)
+  (setq show-bm-tool-bar (if show-bm-tool-bar nil t)))
+
 ;; (tool-bar-add-item "separator" nil 'custom-tool-bar)
 
 ;; bookmark
-;; (when (featurep 'bm)
-;;   (tool-bar-add-item "bm-toggle" 'bm-toggle 'bm-toggle)
-;;   (tool-bar-add-item "bm-next" 'bm-next 'bm-next)
-;;   (tool-bar-add-item "bm-previous" 'bm-previous 'bm-previous)
-;;   (tool-bar-add-item "bm-clear" 'bm-remove-all-current-buffer
-;;                      'bm-remove-all-current-buffer))
+(when (featurep 'bm)
+  (tool-bar-add-item "bm-toggle" 'bm-toggle 'bm-toggle
+                     :visible 'show-bm-tool-bar)
+  (tool-bar-add-item "bm-next" 'bm-next 'bm-next
+                     :visible 'show-bm-tool-bar)
+  (tool-bar-add-item "bm-previous" 'bm-previous 'bm-previous
+                     :visible 'show-bm-tool-bar)
+  (tool-bar-add-item "bm-clear" 'bm-remove-all-current-buffer
+                     'bm-remove-all-current-buffer
+                     :visible 'show-bm-tool-bar))
 
 ;; recent-jump
-(tool-bar-add-item "left-arrow"'recent-jump-jump-backward
+(tool-bar-add-item "recent-backward"'recent-jump-jump-backward
                    'recent-jump-jump-backward
                    :enable '(fboundp 'recent-jump-jump-backward))
-(tool-bar-add-item "right-arrow" 'recent-jump-jump-forward
+(tool-bar-add-item "recent-forward" 'recent-jump-jump-forward
                    'recent-jump-jump-forward
                    :enable '(fboundp 'recent-jump-jump-forward))
 
@@ -46,10 +58,15 @@
 
 ;; (tool-bar-add-item "separator" nil 'toggle)
 
+;; linum
+(tool-bar-add-item "linum" 'global-linum-mode
+                   'global-linum-mode
+                   :visible '(fboundp 'global-linum-mode))
+
 ;; toggle whitespace-mode
 (tool-bar-add-item "whitespace" 'whitespace-mode
                    'whitespace-mode
-                   :enable '(fboundp 'whitespace-mode))
+                   :visible '(fboundp 'whitespace-mode))
 
 ;; toggle ecb
 (tool-bar-add-item "ecb"
@@ -59,15 +76,22 @@
                          (ecb-deactivate)
                        (ecb-activate)))
                    'ecb
-                   :enable '(fboundp 'ecb-activate))
+                   :visible '(fboundp 'ecb-activate))
 
 ;; (tool-bar-add-item "separator" nil 'program)
 
 ;; semantic jump/back
 (tool-bar-add-item "semantic-jump-back" 'semantic-ia-fast-jump-back
-                   'semantic-ia-fast-jump-back)
+                   'semantic-ia-fast-jump-back
+                   :visible (fboundp 'semantic-ia-fast-jump-back))
 (tool-bar-add-item "semantic-jump" 'semantic-ia-fast-jump
-                   'semantic-ia-fast-jump)
+                   'semantic-ia-fast-jump
+                   :visible (fboundp 'semantic-ia-fast-jump))
+
+;; compile/debug
+(tool-bar-add-item "compile" 'compile 'compile)
+(tool-bar-add-item "gdb" 'gdb 'gdb
+                   :visible '(memq major-mode '(c++-mode c-mode objc-mode)))
 
 ;; toggle h/cpp
 (tool-bar-add-item "sourcepair"
@@ -77,12 +101,9 @@
                          (sourcepair-load)
                        (eassist-switch-h-cpp)))
                    'sourcepair
-                   :enable '(or (fboundp 'sourcepair-load)
-                                (fboundp 'eassist-switch-h-cpp)))
+                   :visible '(and (memq major-mode '(c++-mode c-mode objc-mode))
+                                  (or (fboundp 'sourcepair-load)
+                                      (fboundp 'eassist-switch-h-cpp))))
 
-;; compile/debug
-(tool-bar-add-item "compile" 'compile 'compile)
-(tool-bar-add-item "gud/run" 'gdb 'gdb
-                   :visible nil)
 
 (provide 'init-toolbar)
