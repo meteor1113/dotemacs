20b4a0d6fbbf865a9d46e5fb1697eb00322d2736
Meteor Liu <meteor1113@gmail.com>
2011-01-25 17:42:41 +0800

    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- init-basic.el
+++ init-basic.el
@@ -366,7 +366,7 @@ Like eclipse's Ctrl+Alt+F."
 (global-set-key (kbd "ESC <C-f6>") (key-binding [C-M-f6]))
 (global-set-key [f7] '(lambda () (interactive) (compile compile-command)))
 (global-set-key [header-line double-mouse-1] 'kill-this-buffer)
-(global-set-key [header-line mouse-3] 'delete-other-windows)
+(global-set-key [header-line mouse-3] 'kill-this-buffer)
 
 
 ;;; special mode setting
