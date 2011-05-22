de6b8ec1a943e0a1fce9b5398ab272ff8e79e744
Meteor Liu <meteor1113@gmail.com>
2011-05-22 23:34:08 +0800

    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- init-basic.el
+++ init-basic.el
@@ -186,6 +186,17 @@
           (file-cache-add-directory-recursively "/usr/include/c++")
           (file-cache-add-directory-recursively "/usr/local/include")))
 
+;; diary
+;; (setq diary-file "~/emacs.d/diary")
+;; (add-hook 'diary-hook 'appt-make-list)
+;; (setq appt-display-format 'window)
+;; (setq appt-display-mode-line t)
+;; (setq appt-display-diary nil)
+(setq appt-display-duration (* 60 60))
+(appt-activate 1)
+;; (diary 0)
+;; http://www.emacswiki.org/emacs/OrgMode-OSD
+
 ;; misc
 (setq inhibit-startup-message t)        ; for no desktop
 (require 'generic-x nil 'noerror)
