631eaf60576e820778e89700a8a3fd2854ce5080
Meteor Liu <meteor1113@gmail.com>
2011-05-23 10:59:53 +0800

    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- init-misc.el
+++ init-misc.el
@@ -167,12 +167,13 @@
   (set-face-background 'highlight-symbol-face
                        (if window-system "gray83" "blue"))
   ;; (if (eq frame-background-mode 'dark) "blue" "gray83"))
-  (add-hook 'after-make-frame-functions
-            (lambda (frame)
-              (with-selected-frame frame
-                (set-face-background 'highlight-symbol-face
-                                     (if window-system "gray83" "blue")
-                                     frame))))
+  (if (daemonp)
+      (add-hook 'after-make-frame-functions
+                (lambda (frame)
+                  (with-selected-frame frame
+                    (set-face-background 'highlight-symbol-face
+                                         (if window-system "gray83" "blue")
+                                         frame)))))
   (defun highlight-symbol-temp-highlight () ; Hack for emacs-21
     "Highlight the current symbol until a command is executed."
     (when highlight-symbol-mode
