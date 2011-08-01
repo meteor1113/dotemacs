a2f5ac41b008755f07827560fc36394e1930f0bc
Meteor Liu <meteor1113@gmail.com>
2011-08-01 22:15:39 +0800

    Add .dir-locals.el sample.
    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

---
+++ sample/.dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 4)
+         (comment-column . 32)
+         (fill-column . 80)))
+ (c-mode . ((c-file-style . "stroustrup")))
+ (java-mode . ((c-file-style . "BSD")))
+ ("src/imported"
+  . ((nil . ((change-log-default-name . "ChangeLog.local"))))))
