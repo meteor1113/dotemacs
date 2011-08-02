883bdb55bbca68903cf40e3cb59cea2b8b6b4b11
Meteor Liu <meteor1113@gmail.com>
2011-08-02 14:38:26 +0800

    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- init-site.el
+++ init-site.el
@@ -246,6 +246,7 @@ If NOSET is non-nil, don't bother autoloading LOAD when setting the variable."
                                      "left8"
                                      "left-analyse"
                                      "left-symboldef")
+        ;; ecb-windows-width 0.25
         ecb-compile-window-height 0.2
         ecb-compile-window-width 'edit-window
         ecb-compile-window-temporally-enlarge 'after-selection
