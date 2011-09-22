04161111441d68537f37b1693317a42e8dc6f292
Meteor Liu <meteor1113@gmail.com>
2011-09-22 20:50:03 +0800

    comment if0 in c/c++-mode.
    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- init-basic.el
+++ init-basic.el
@@ -709,6 +709,35 @@ Like eclipse's Ctrl+Alt+F."
   (ignore-errors (imenu-add-menubar-index)))
 
 (add-hook 'c-mode-common-hook 'program-common-function)
+(defun my-c-mode-font-lock-if0 (limit)
+  (save-restriction
+    (widen)
+    (save-excursion
+      (goto-char (point-min))
+      (let ((depth 0) str start start-depth)
+        (while (re-search-forward "^\\s-*#\\s-*\\(if\\|else\\|endif\\)" limit 'move)
+          (setq str (match-string 1))
+          (if (string= str "if")
+              (progn
+                (setq depth (1+ depth))
+                (when (and (null start) (looking-at "\\s-+0"))
+                  (setq start (match-end 0)
+                        start-depth depth)))
+            (when (and start (= depth start-depth))
+              (c-put-font-lock-face start
+                                    (match-beginning 0)
+                                    'font-lock-comment-face)
+              (setq start nil))
+            (when (string= str "endif")
+              (setq depth (1- depth)))))
+        (when (and start (> depth 0))
+          (c-put-font-lock-face start (point) 'font-lock-comment-face)))))
+  nil)
+(defun my-c-mode-common-hook-if0 ()
+  (font-lock-add-keywords
+   nil
+   '((my-c-mode-font-lock-if0 (0 font-lock-comment-face prepend))) 'add-to-end))
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook-if0)
 
 (add-to-list 'auto-mode-alist '("\\.[ch]\\'" . c++-mode))
 (add-hook 'c-mode-hook (lambda () (c-set-style "stroustrup")))
