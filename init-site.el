18953a3e7068c2b8acbc46c135431fbec36b61df
Liu Xin <meteor1113@gmail.com>
2015-03-11 08:47:38 +0800

    set ecb-source-path
    Signed-off-by: Liu Xin <meteor1113@gmail.com>

--- init-site.el
+++ init-site.el
@@ -243,7 +243,7 @@ If NOSET is non-nil, don't bother autoloading LOAD when setting the variable."
 (when (fboundp 'ecb-minor-mode)
   (defvar ecb-minor-mode nil)
   (setq ecb-primary-secondary-mouse-buttons 'mouse-1--C-mouse-1
-        ecb-source-path '("/")
+        ecb-source-path (quote ("/" ("c:" "c:")))
         ecb-layout-name 'left3
         ecb-toggle-layout-sequence '("left3"
                                      "left8"
