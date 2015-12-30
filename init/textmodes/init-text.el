d74c6f937fa9b46f6368aab0ab45ecf6c732a19f
Liu Xin <meteor1113@qq.com>
2015-12-30 23:04:20 +0800

    Signed-off-by: Liu Xin <meteor1113@qq.com>

--- init/textmodes/init-text.el
+++ init/textmodes/init-text.el
@@ -10,12 +10,13 @@
 ;; @URL http://git.oschina.net/meteor1113/dotemacs
 
 ;; text-mode
-(let ((text-imenu-generic-expression
-       `((nil ,"^ \\{0,4\\}\\([一二三四五六七八九十]+[、. )]\\)+ *[^,。，]+?$" 0)
-         (nil ,"^ \\{0,4\\}\\([0-9]+[、. )]\\)+ *[^,。，]+?$" 0))))
-  (add-hook 'text-mode-hook
-            '(lambda ()
-               (setq imenu-generic-expression text-imenu-generic-expression)
-               (imenu-add-menubar-index))))
+(setq text-imenu-generic-expression
+      `((nil ,"^ \\{0,4\\}\\([一二三四五六七八九十]+[、. )]\\)+ *[^,。，]+?$" 0)
+        (nil ,"^ \\{0,4\\}\\([0-9]+[、. )]\\)+ *[^,。，]+?$" 0)))
+
+(add-hook 'text-mode-hook
+          '(lambda ()
+             (setq imenu-generic-expression text-imenu-generic-expression)
+             (imenu-add-menubar-index)))
 
 (provide 'init-text)
