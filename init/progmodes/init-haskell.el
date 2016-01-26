63a96a2ab54bf9c7c12b172c6408098458af8f9c
Liu Xin <meteor1113@qq.com>
2016-01-26 23:04:12 +0800

    Add package ghc and company-ghc.
    Signed-off-by: Liu Xin <meteor1113@qq.com>

--- init/progmodes/init-haskell.el
+++ init/progmodes/init-haskell.el
@@ -20,6 +20,25 @@
              (haskell-indentation-mode)
              (interactive-haskell-mode +1)))
 
+;; ghc
+(setq ghc-display-error 'minibuffer)
+(add-hook 'haskell-mode-hook
+          '(lambda ()
+             (ignore-errors (ghc-init))))
+
+;; company-ghc
+(add-hook 'haskell-mode-hook
+          '(lambda ()
+             (when (fboundp 'company-mode)
+               (company-mode 1)
+               (define-key haskell-mode-map (kbd "M-n") 'company-select-next)
+               (define-key haskell-mode-map "\ep" 'company-select-previous)))
+          'append)
+(eval-after-load "company"
+  '(progn
+     (when (fboundp 'company-ghc)
+       (add-to-list 'company-backends 'company-ghc))))
+
 (provide 'init-haskell)
 
 ;;; init-haskell.el ends here
