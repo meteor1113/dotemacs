a5d6572aa49f4018e3d547e5a0d80914a509dc92
Liu Xin <meteor1113@qq.com>
2016-01-29 09:27:48 +0800

    Remove package go-snippets.
    Signed-off-by: Liu Xin <meteor1113@qq.com>

--- elpa/go-snippets-20151121.2257/go-snippets-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; go-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "go-snippets" "go-snippets.el" (22173 63386
-;;;;;;  0 0))
-;;; Generated autoloads from go-snippets.el
-
-(autoload 'go-snippets-initialize "go-snippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(go-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("go-snippets-pkg.el") (22173 63386 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-snippets-autoloads.el ends here
