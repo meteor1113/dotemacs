538a1fb38223ba413a6d4cdd9db964b399b43beb
Liu Xin <meteor1113@qq.com>
2017-04-15 15:26:51 +0800

    Key binding for os x.

--- init/init-keybinding.el
+++ init/init-keybinding.el
@@ -118,7 +118,9 @@
 
 (when (eq system-type 'darwin)
   (global-set-key (kbd "<s-left>") 'move-beginning-of-line)
-  (global-set-key (kbd "<s-right>") 'move-end-of-line))
+  (global-set-key (kbd "<s-right>") 'move-end-of-line)
+  (global-set-key (kbd "<s-up>") 'beginning-of-buffer)
+  (global-set-key (kbd "<s-down>") 'end-of-buffer))
 
 (when (eq system-type 'aix)
   (global-set-key (kbd "C-d") 'backward-delete-char-untabify)
